da1d6afea0b50dd14754d8a91660f674515e4716
Hikari-huk <ghikariao974@gmail.com>
2022-08-19 01:14:58 +0900

    中間テーブルと在庫コントローラーとStockページ作成

---
+++ app/Http/Controllers/StockController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StockController extends Controller
+{
+    //借用画面の表示
+    public function create(){
+        
+    }
+    
+    //借用の処理
+    public function store(){
+        
+    }
+    
+    //返却画面の表示
+    public function edit(){
+        
+    }
+    
+    //延長の処理
+    public function update(){
+        
+    }
+    
+    //返却の処理
+    public function delete(){
+        
+    }
+    
+}
---
+++ database/migrations/2022_08_18_150608_create_item_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_user', function (Blueprint $table) {
+            $table->unsignedBigInteger("item_id");
+            $table->unsignedBigInteger("user_id");
+            $table->primary(['item_id','user_id']);
+            $table->date("borrowed_at");
+            $table->date("returned_at");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_user');
+    }
+};
